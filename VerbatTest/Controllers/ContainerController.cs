da43d966ae294e08edad6e696036ee969cb42e5a
aswathyvm8292 <aswathyvm8292@gmail.com>
2024-09-22 17:52:14 +0530

    Add project files.

---
+++ VerbatTest/Controllers/ContainerController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VerbatTest.Models;
+
+namespace VerbatTest.Controllers
+{
+    [Authorize]
+    public class ContainerController : Controller
+    {
+        private readonly ContainerManagementEntities DbContext = new ContainerManagementEntities();
+        public ActionResult Index()
+        {
+            try
+            {
+                List<Container> container = DbContext.Containers.ToList();
+                return View(container);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                Container container = DbContext.Containers.Single(containers => containers.ContainerId == id);
+                return View(container);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+        public ActionResult Create()
+        {
+            try
+            {
+                List<SelectListItem> statusList = new List<SelectListItem>();
+                statusList.Add(new SelectListItem { Text = "Select status", Value = "", Selected = true });
+                statusList.Add(new SelectListItem { Text = "Transit", Value = "Transit" });
+                statusList.Add(new SelectListItem { Text = "At Dock", Value = "At Dock" });
+                statusList.Add(new SelectListItem { Text = "Delivered", Value = "Delivered" });
+                ViewBag.Status = statusList;
+                return View();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Container container, HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file != null)
+                {
+                    int maxSize = 5 * 1024 * 1024;
+                    if (file.ContentLength > maxSize)
+                    {
+                        ModelState.AddModelError("file", "File size must not exceed 5 MB.");
+                        ViewBag.Message = "File size must not exceed 5 MB.";
+                    }
+
+                    string fileExtension = Path.GetExtension(file.FileName).ToLowerInvariant();
+                    if (fileExtension != ".pdf")
+                    {
+                        ModelState.AddModelError("file", "Invalid file type. Only .pfd are allowed.");
+                        ViewBag.Message = "Invalid file type. Only .pfd are allowed.";
+
+                    }
+                }
+                if (ModelState.IsValid)
+                {
+                    if (DbContext.Containers.Any(x => x.ContainerNumber == container.ContainerNumber))
+                    {
+                        ViewBag.Message = "Container Number is already existed";
+                        return View();
+                    }
+                    else
+                    {
+                        if (file != null && file.ContentLength > 0)
+                        {
+                            string fileName = Path.GetFileName(file.FileName);
+                            string path = Path.Combine(Server.MapPath("~/FileStore"), fileName);
+                            file.SaveAs(path);
+                            container.FilePath = fileName;
+                        }
+                        DbContext.Containers.Add(container);
+                        DbContext.SaveChanges();
+                        return RedirectToAction("Index", "Container");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+            }
+            return View(container);
+        }
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                if (id > 0)
+                {
+                    var contId = DbContext.Containers.Where(x => x.ContainerId == id).FirstOrDefault();
+                    if (contId != null)
+                    {
+                        DbContext.Entry(contId).State = EntityState.Deleted;
+                        DbContext.SaveChanges();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult Download(string FilePath)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(FilePath))
+                {
+                    return View();
+                }
+                else
+                {
+                    string Path = System.Configuration.ConfigurationManager.AppSettings["FileStore"] + FilePath;
+                    if (!System.IO.File.Exists(Path))
+                    {
+                        return HttpNotFound();
+                    }
+                    return File(Path, "application/pdf");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ VerbatTest/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using VerbatTest.Models;
+
+namespace VerbatTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ContainerManagementEntities DbContext = new ContainerManagementEntities();
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                bool IsValidUser = DbContext.Users
+               .Any(u => u.Username.ToLower() == user
+               .Username.ToLower() && u.Password == user.Password);
+
+                if (IsValidUser)
+                {
+                    FormsAuthentication.SetAuthCookie(user.Username, false);
+                    return RedirectToAction("Index", "Container");
+                }
+            }
+            ModelState.AddModelError("", "invalid Username or Password");
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
